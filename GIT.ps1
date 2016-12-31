6c517e22ba5886fc2ca0811cd4a3e17223c9e416
iykedairo <okeysajogwuoke@gmail.com>
2016-12-31 12:55:18 -0800

    Auto Commits

--- GIT.ps1
+++ GIT.ps1
@@ -1,5 +1,7 @@
 ﻿cd "C:\WEB\WEB"
+cls
 git init
+set GIT_CURL_VERBOSE=1
 git config user.name iykedairo
 git config user.email okeysajogwuoke@gmail.com
 git config --global user.name iykedairo
@@ -7,7 +9,7 @@ git clone https://github.com/iykedairo/Lib.git
 git remote add Local https://github.com/iykedairo/Lib.git
 git remote set-url --add Local https://github.com/iykedairo/Lib.git
 git remote -v
-add git add --all :/
+git add git add --all :/
 git commit -am "Auto Commits"
 git pull Local master --allow-unrelated-histories
 git push Local master
