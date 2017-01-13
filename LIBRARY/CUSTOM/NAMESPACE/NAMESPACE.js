97e615677cf9bea7321ca41dfd3baee955c5b1b6
iykedairo <okeysajogwuoke@gmail.com>
2017-01-12 22:57:36 -0800

    12-01-2017:22.57hrs

--- LIBRARY/CUSTOM/NAMESPACE/NAMESPACE.js
+++ LIBRARY/CUSTOM/NAMESPACE/NAMESPACE.js
@@ -144,3 +144,30 @@ console.log('test 4', myNS);
 //success, myApp.hello.world.test3 is now 'hello again'
 
 console.groupEnd();
+
+var namespace = (function () {
+
+    // defined within the local scope
+    var privateMethod1 = function () { /* ... */ }
+    var privateMethod2 = function () { /* ... */ }
+    var privateProperty1 = 'foobar';
+
+    return {
+        // the object literal returned here can have as many
+        // nested depths as you wish, however as mentioned,
+        // this way of doing things works best for smaller,
+        // limited-scope applications in my personal opinion
+        publicMethod1: privateMethod1,
+
+        //nested namespace with public properties
+        properties:{
+            publicProperty1: privateProperty1
+        },
+
+        //another tested namespace
+        utils:{
+            publicMethod2: privateMethod2
+        }
+        ...
+    }
+})();
